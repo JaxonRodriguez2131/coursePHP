362c72279ea3af964233dd648de4aac2c0d3ec6b
JaxonRodriguez2131 <jalexandercardona31@gmail.com>
2023-05-24 16:22:27 -0500

    Declaracion de variables y operadores

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+    
+    $nombre = "jaxon";
+    $Nombre = "alexander";
+    define('CURSO',['PHP 8 ' , 'mySQL']); //funcion para definir una constante
+    $numero = 3;
+    $numero2 = 3.2;
+    $informacion = 'hola mi nombre es "jaxon" y tengo "29" años';
+    $a = 3;
+    $b = 7;
+    $c = $a + $b;
+
+    $a += $b; // a = a+b; 
+
+    echo $nombre;
+    echo "mi sistema operativo es " . PHP_OS;
+    echo "<br/>";
+    echo "directorio" . __FILE__;
+    echo $informacion;
+    echo $c;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+        <h1>
+            <?php echo "Hola mi nombre es " .$Nombre; //Imprimir una variable
+                  echo CURSO[1]; //imprimir una constante
+            ?>
+        </h1>
+    </section>
+</body>
+</html>
