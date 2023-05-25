7113bccb9773ebef6722f67da2b80147c31a6add
JaxonRodriguez2131 <jalexandercardona31@gmail.com>
2023-05-24 23:20:27 -0500

    funciones string, arrays uni y bidimensonales y algunas de sus funciones

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    
+    declare(strict_types=1); //esta funcion sirve para cuando definimos un tipado fuerte 
+
     $nombre = "jaxon";
     $Nombre = "alexander";
     define('CURSO',['PHP 8 ' , 'mySQL']); //funcion para definir una constante
@@ -125,9 +126,141 @@
     }
     echo "<br/>";
     $answer = factorial(10);
-    echo $answer;
+    echo $answer . "<br/>";
+
+    //Ambito de variables, locales y globales
+
+    //las funciones se consideran bloques de codigo independientes, es decir no se pueden usar
+    // variables que se hayan definido fuera de ellas
+    //para acceder a las variables globales dentro de funciones usamos el atributo global y asi podemos
+    // modificar su valor
+
+    $f = 5;
+    $g = 10;
+    function variable(&$n){ // cuando agregamos este simbolo & no solo pasamos el valor de la variables
+        $n+=10;             // si no todos sus atributos y asi los cambios que hagamos dentro de una funcion
+                            // se veran reflejados de forma global 
+    }
+
+    variable($f);
+    variable($g);
+
+    echo $f . "<br/>";
+    echo $g;
+
+    //en caso que desconozcamos la cantidad de atributos que debe recibir una funcion la declaramos de la 
+    // siguien manera
+
+    function concatenar(...$palabras){ //esto lo que hace es que junta todos los valores en un array 
+            $resultado = "" ;
+            foreach ($palabras as $palabra) {
+                $resultado = $resultado . $palabra . " ";
+            }
+            echo $resultado;
+    }
+
+    concatenar("curso" , "php" , "desde" , "cero");
+
+
+    $steve = 5 ; 
+
+    function sumarEnteros(int $entero1 , int $entero2):int | float //sirve para declara que se devuelva un tipo
+    {                                                      // de dato en especifico
+        return $entero1 + $entero2;
+    }
+
+    // podemos especificar tipos de datos int, float,string,boolean,array,object,null
+
+    $resultadosumaE = sumarEnteros($f , $steve);
+    echo $resultadosumaE;
+
+
+    //cadenas
+
+    $cadena = "jaxon alexander rodriguez cardona";
+    $cadena1 = "pepe mujica";
+
+    echo $cadena[1] . "<br/>"; //imprime un caracter especifico de una cadena
+    echo mb_strlen($cadena); // devuelve la longitud de una cadena
+    echo strpos($cadena,'i');// devuelve la posicion del caracter que le demos a buscar dentro de la cadena
+    echo str_contains($cadena,'jaxon'); // devuelve falso o verdadero si encuentra una cedan dentro de otra cadena
+    echo str_starts_with($cadena , 'jaxon'); // devuelve f o v si una cadena empieza con el parametro que le demos
+    echo str_ends_with($cadena , 'jaxon'); // devuelve f o v si una cadena finaliza con el parametro que le demos
+    
+    //funcion para comparar dos cadenas de texto
+    if(strcmp($cadena1 , $cadena) == 0 ){
+        echo "las dos cadenas son iguales";
+    }else{
+        echo "las cadenas son diferentes";
+    }
+
+    substr($cadena1 , 3 , 7); //funcion para extraer una subcadena, le podremos indicar el indice inicio
+                            // y fun de la subcadena
+    echo str_replace("mujica" , "osvaldo" , $cadena1); //remplazar caracteres en una cadena
+    echo strtolower($cadena1); // convierte una cadena a minisculas
+    echo strtoupper($cadena1); // convierte una cadena en mayuscula
+    echo ucwords($cadena); //convierte la primera letra de cada palabra en mayuscula
+
+    //Arrays 
+
+    $datos = [
+        'nombre' => 'jaxon',
+        'email' => 'alexander'
+    ];
+
+    echo $datos['nombre'];
+
+    //de esta forma modificamos su indice para que sea mas facil identificar sus elementos
+    // es decir no busca por numeros si no por el nombre que le dimos al indice
+    
+
+    // Arrays multidimensionales
+
+    $data = [
+        [
+            'nombre' => 'camilo',
+            'email' => 'valencia' 
+        ],
+
+        [
+            'nombre' => 'mary',
+            'email' => 'cardona'
+        ],
+
+        [
+            'nombre' => 'pepe ',
+            'email' => 'mujica'
+        ]
+
+    ];
+
+    foreach($data as $datos){
+        echo $datos['nombre'] . "<br/>";
+    }
+
+    //funciones para los arrays 
+
+    $variablem;
+    $variablej;
+    $variablek;
+
+    $datosmix = [1,2,3];
+
+    list($variablem,$variablej,$variablek) = $datosmix; // esta funcion sirve para almacenar el indice de un array en una variable
+    $datosmix = range(10,20); //almacena en el array un rango de numeros
+    echo count($datosmix); //cuenta los elementos de un array
+
+    if(in_array(11,$datosmix)){ //funcion para buscar un elemento en un array
+        echo "el numero 11 de encuentea en el array";
+    }
+
+    unset($datosmix[4]); //elimina un elemento de un array
+
+
 
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
